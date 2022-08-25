4579f89f67324ab81b1a70f3bc07c15af2d43a8d
Aqil Zeeshan <azeeshan@freewheel.com>
2022-08-25 21:14:05 +0100

    Simple Web Server

--- one-server/main.tf
+++ one-server/main.tf
@@ -3,10 +3,42 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example-1"
   }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP of the Instance"
 }
